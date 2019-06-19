27a15e75d9a88bbb88f6f41ae8f15bc3b7fe1993
Unknown <dmr_2910@hotmail.es>
2019-06-19 15:42:34 +0200

    vista ejemplo del fondo de la página principal

--- App/resources/views/home.blade.php
+++ App/resources/views/home.blade.php
@@ -1,8 +1,20 @@
 @extends('layouts.app')
 @section('content')
-<div class="container-fluid">
-    <div class="row justify-content-center">
-      <td><a href="{{ action('EjercicioController@index', ['id' => 1]) }}"> ejericicio 1</a></td>
-    </div>
+<div class="container row">
+  <div class="col-2">
+    <a href="{{ action('EjercicioController@index', ['id' => 1]) }}"> Ver ejercicios resueltos</a>
+  </div>
+  <div class="col-2">
+    <a href="{{ action('EjercicioController@index', ['id' => 1]) }}"> ejericicio 1</a>
+  </div>
+  <div class="col-4 justify-content-center text-center">
+    <a href="{{ action('EjercicioController@index', ['id' => 1]) }}"> ejericicio 1</a>
+  </div>
+  <div class="col-2">
+    <a href="{{ action('EjercicioController@index', ['id' => 1]) }}"> ejericicio 1</a></td>
+  </div>
+  <div class="col-2">
+    <a href="{{ action('EjercicioController@index', ['id' => 1]) }}"> Enviar sugerencia o problema</a>
+  </div>
 </div>
 @endsection
--- App/resources/views/layouts/app.blade.php
+++ App/resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="background-image: url('imagenes/fondoTfg.jpg');">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -22,11 +22,42 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <main class="row">
-          <div class="col-1"></div>
-          <div class="col-11 mt-4">@yield('content')</div>
-        </main>
+  <nav class="navbar navbar-expand-md bg-header fixed-top p-0">
+    <div class="col-md-2 offset-md-9 text-center">
+      <ul class="navbar-nav justify-content-end mr-3">
+        <!-- Authentication Links -->
+        @auth
+        <li class="nav-item dropdown">
+          <a id="navbarDropdown" class="nav-link dropdown-toggle menu" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+          {{ Auth::user()->name }}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ url('logsRefresh') }}" id="refrescarLogs">Editar Usuario</a>
+            <a class="dropdown-item" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+            </form>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link menu" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            Salir <i class="fa fa-sign-out-alt"></i>
+          </a>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+          @csrf
+          </form>
+        </li>
+        @endauth
+      </ul>
+    </div>
+  </nav>
+    <div id="app" class="h-100" style=" ">
+          <div id="home" class="h-100">@yield('content')</div>
     </div>
     @yield('scripts')
 </body>
