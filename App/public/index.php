1cc255226d717113b00c4814974cd1f26cee54ca
Unknown <dmr_2910@hotmail.es>
2020-04-13 14:02:20 +0200

    pruebaCors

--- App/public/index.php
+++ App/public/index.php
@@ -8,6 +8,12 @@
  */
 
 define('LARAVEL_START', microtime(true));
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
 
 /*
 |--------------------------------------------------------------------------
