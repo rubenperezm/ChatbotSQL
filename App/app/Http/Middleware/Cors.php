e78d8a6ef8d05598ec76ffccb4af2256014dbc81
Unknown <dmr_2910@hotmail.es>
2020-04-13 20:31:52 +0200

    Cors prueba

--- App/app/Http/Middleware/Cors.php
+++ App/app/Http/Middleware/Cors.php
@@ -7,10 +7,10 @@ class Cors
   {
     return $next($request)
        //Url a la que se le dará acceso en las peticiones
-      ->header("Access-Control-Allow-Origin", "*")
+      ->header("Access-Control-Allow-Origin", "http://ec2-52-90-113-78.compute-1.amazonaws.com:3000")
       //Métodos que a los que se da acceso
       ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
       //Headers de la petición
-      ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+      ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth");
   }
 }
