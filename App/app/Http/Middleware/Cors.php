e501a0dfc2bd3691ed731b92ffe72c1e540b1d48
Unknown <dmr_2910@hotmail.es>
2020-04-13 18:03:40 +0200

    prueba bot en pro

---
+++ App/app/Http/Middleware/Cors.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Middleware;
+use Closure;
+class Cors
+{
+  public function handle($request, Closure $next)
+  {
+    return $next($request)
+       //Url a la que se le dará acceso en las peticiones
+      ->header("Access-Control-Allow-Origin", "*")
+      //Métodos que a los que se da acceso
+      ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+      //Headers de la petición
+      ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+  }
+}
--- App/routes/api.php
+++ App/routes/api.php
@@ -16,7 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-//Route::resource('ejercicio', 'EjercicioController');
-Route::get('/apiEjercicio/store','apiEjercicioController@store')->name('store');
-Route::get('/apiEjercicio/show/{id}','apiEjercicioController@show')->name('show');
+Route::group(['middleware' => ['cors']], function () {
+  //Route::resource('ejercicio', 'EjercicioController');
+  Route::get('/apiEjercicio/store','apiEjercicioController@store')->name('store');
+  Route::get('/apiEjercicio/show/{id}','apiEjercicioController@show')->name('show');
+});
