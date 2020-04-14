fe08cf5b295819df29a6314e6fdf361644232591
Unknown <dmr_2910@hotmail.es>
2020-04-14 11:54:55 +0200

    arreglando nombre variable

--- botTFG/app.js
+++ botTFG/app.js
@@ -23,7 +23,7 @@ var cors = require('cors')
 
 var app = express();
 app.use(cors());
-router.use(function(req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
